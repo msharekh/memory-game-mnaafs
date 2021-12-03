a11f258b287e5178d56e087b920fe07413535ff5
Mishal Alsharekh <mishal.sharekh@gmail.com>
2021-12-03 10:48:38 +0300

    need to complete

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
+
 const numbers = [
     {
-        name: '2',
-        image:'2'
+        name: 'two',
+        img: 'images/two.png'
+    },
+    {
+        name: 'five',
+        img: 'images/five.png'
+    },
+    {
+        name: 'seven',
+        img: 'images/seven.png'
+    },
+    {
+        name: 'nine',
+        img: 'images/nine.png'
     }
 ];
+
+numbers.sort(function () {
+    return 0.5 * Math.random();
+});
+
+const grid = document.querySelector('.grid');
+
+const resultDisplay = document.querySelector('#result');
+
+let cardsChosen = [];
+let cardsChosenId = [];
+let cardsWon= [];
+
+createBoard();
+
+function createBoard() {
+    for (let i = 0; i < numbers.length; i++) {
+        let card = document.createElement('img');
+        card.setAttribute('src','images/blank.png');
+        card.setAttribute('data-id',i);
+        //card.addEventListener('click',flipCard);
+        grid.appendChild(card);
+    }
+}
+
+
